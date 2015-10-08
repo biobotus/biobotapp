5735bf7446b53de224b450ad3f11bc745f366f94
David Goulet <david.goulet4@usherbrooke.ca>
2015-10-08 15:07:43 -0400

    Added DBManager and changed dataset name

---
+++ application/Model/Data/DBManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioBotApp.Model.Data
+{
+    class DBManager
+    {
+        private static DBManager instance;
+        private Data.BioBotDataset projectDataset { get; set; }
+
+        private DBManager()
+        {
+            projectDataset = new BioBotDataset();
+        }
+
+        public static DBManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DBManager();
+                }
+                return instance;
+            }
+        }
+    }
+}
