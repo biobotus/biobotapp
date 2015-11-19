83c3bab7cd4c20c7b2f5abc6d68237a404580436
rizt2301 <Thibault.Rizzo@usherbrooke.ca>
2015-11-19 15:09:47 -0500

    Add drag and drop effect on deck features

--- application/View/Deck/DeckView.Designer.cs
+++ application/View/Deck/DeckView.Designer.cs
@@ -29,171 +29,47 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.deck = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.pkidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.fkobjecttypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.deckxDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.deckyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.rotationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.activatedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.fkobjectDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.bsBiobot = new System.Windows.Forms.BindingSource(this.components);
             this.bioBotDataSets = new BioBotApp.Model.Data.BioBotDataSets();
+            this.bsObject = new System.Windows.Forms.BindingSource(this.components);
             this.taObject = new BioBotApp.Model.Data.BioBotDataSetsTableAdapters.bbt_objectTableAdapter();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bsBiobot)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bioBotDataSets)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsObject)).BeginInit();
             this.SuspendLayout();
-            // 
-            // deck
-            // 
-            this.deck.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.deck.Location = new System.Drawing.Point(46, 48);
-            this.deck.Name = "deck";
-            this.deck.Size = new System.Drawing.Size(317, 234);
-            this.deck.TabIndex = 0;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.pkidDataGridViewTextBoxColumn,
-            this.fkobjecttypeDataGridViewTextBoxColumn,
-            this.deckxDataGridViewTextBoxColumn,
-            this.deckyDataGridViewTextBoxColumn,
-            this.rotationDataGridViewTextBoxColumn,
-            this.activatedDataGridViewTextBoxColumn,
-            this.descriptionDataGridViewTextBoxColumn,
-            this.fkobjectDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.bsBiobot;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.dataGridView1.Location = new System.Drawing.Point(466, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(404, 365);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // pkidDataGridViewTextBoxColumn
-            // 
-            this.pkidDataGridViewTextBoxColumn.DataPropertyName = "pk_id";
-            this.pkidDataGridViewTextBoxColumn.HeaderText = "pk_id";
-            this.pkidDataGridViewTextBoxColumn.Name = "pkidDataGridViewTextBoxColumn";
-            // 
-            // fkobjecttypeDataGridViewTextBoxColumn
-            // 
-            this.fkobjecttypeDataGridViewTextBoxColumn.DataPropertyName = "fk_object_type";
-            this.fkobjecttypeDataGridViewTextBoxColumn.HeaderText = "fk_object_type";
-            this.fkobjecttypeDataGridViewTextBoxColumn.Name = "fkobjecttypeDataGridViewTextBoxColumn";
-            // 
-            // deckxDataGridViewTextBoxColumn
-            // 
-            this.deckxDataGridViewTextBoxColumn.DataPropertyName = "deck_x";
-            this.deckxDataGridViewTextBoxColumn.HeaderText = "deck_x";
-            this.deckxDataGridViewTextBoxColumn.Name = "deckxDataGridViewTextBoxColumn";
-            // 
-            // deckyDataGridViewTextBoxColumn
-            // 
-            this.deckyDataGridViewTextBoxColumn.DataPropertyName = "deck_y";
-            this.deckyDataGridViewTextBoxColumn.HeaderText = "deck_y";
-            this.deckyDataGridViewTextBoxColumn.Name = "deckyDataGridViewTextBoxColumn";
-            // 
-            // rotationDataGridViewTextBoxColumn
-            // 
-            this.rotationDataGridViewTextBoxColumn.DataPropertyName = "rotation";
-            this.rotationDataGridViewTextBoxColumn.HeaderText = "rotation";
-            this.rotationDataGridViewTextBoxColumn.Name = "rotationDataGridViewTextBoxColumn";
-            // 
-            // activatedDataGridViewTextBoxColumn
-            // 
-            this.activatedDataGridViewTextBoxColumn.DataPropertyName = "activated";
-            this.activatedDataGridViewTextBoxColumn.HeaderText = "activated";
-            this.activatedDataGridViewTextBoxColumn.Name = "activatedDataGridViewTextBoxColumn";
-            // 
-            // descriptionDataGridViewTextBoxColumn
-            // 
-            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "description";
-            this.descriptionDataGridViewTextBoxColumn.HeaderText = "description";
-            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
-            // 
-            // fkobjectDataGridViewTextBoxColumn
-            // 
-            this.fkobjectDataGridViewTextBoxColumn.DataPropertyName = "fk_object";
-            this.fkobjectDataGridViewTextBoxColumn.HeaderText = "fk_object";
-            this.fkobjectDataGridViewTextBoxColumn.Name = "fkobjectDataGridViewTextBoxColumn";
-            // 
-            // bsBiobot
-            // 
-            this.bsBiobot.DataMember = "bbt_object";
-            this.bsBiobot.DataSource = this.bioBotDataSets;
             // 
             // bioBotDataSets
             // 
             this.bioBotDataSets.DataSetName = "BioBotDataSets";
             this.bioBotDataSets.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
+            // bsObject
+            // 
+            this.bsObject.DataMember = "bbt_object";
+            this.bsObject.DataSource = this.bioBotDataSets;
+            // 
             // taObject
             // 
             this.taObject.ClearBeforeFill = true;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(288, 304);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(404, 194);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // DeckView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.deck);
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Name = "DeckView";
-            this.Size = new System.Drawing.Size(870, 365);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bsBiobot)).EndInit();
+            this.Size = new System.Drawing.Size(547, 363);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.DeckView_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.DeckView_DragEnter);
             ((System.ComponentModel.ISupportInitialize)(this.bioBotDataSets)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsObject)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel deck;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn pkidDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn fkobjecttypeDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn deckxDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn deckyDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn rotationDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn activatedDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn fkobjectDataGridViewTextBoxColumn;
-        private System.Windows.Forms.BindingSource bsBiobot;
         private Model.Data.BioBotDataSets bioBotDataSets;
+        private System.Windows.Forms.BindingSource bsObject;
         private Model.Data.BioBotDataSetsTableAdapters.bbt_objectTableAdapter taObject;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
     }
 }
--- application/View/Deck/DeckView.cs
+++ application/View/Deck/DeckView.cs
@@ -14,47 +14,69 @@ namespace BioBotApp.View.Deck
 {
     public partial class DeckView : UserControl, IDeckView
     {
-        private BioBotDataSets.bbt_objectRow _objectRow;
+        private BioBotDataSets.bbt_propertyRow length = DBManager.Instance.projectDataset.bbt_property.Where(p => p.description == "length").First();
+        private BioBotDataSets.bbt_propertyRow width = DBManager.Instance.projectDataset.bbt_property.Where(p => p.description == "width").First();
+
         public DeckView()
         {
             InitializeComponent();
-            this.bsBiobot.DataMember = "bbt_object";
-            this.bsBiobot.DataSource = Model.Data.DBManager.Instance.projectDataset;
+            this.bsObject.DataMember = "bbt_object";
+            this.bsObject.DataSource = DBManager.Instance.projectDataset;
+            BioBotDataSets.bbt_propertyRow length = DBManager.Instance.projectDataset.bbt_property.Where(p => p.description == "length").First();
+            BioBotDataSets.bbt_propertyRow width = DBManager.Instance.projectDataset.bbt_property.Where(p => p.description == "width").First();
+            BioBotDataSets.bbt_objectRow deck = DBManager.Instance.projectDataset.bbt_object.Where(p => p.description == "deck" && p.activated == "1").First();
+            BioBotDataSets.bbt_object_propertyRow rowWidth = DBManager.Instance.projectDataset.bbt_object_property.Where(p => p.fk_object_type_id == deck.pk_id && p.fk_properties_id == length.pk_id).First();
+            BioBotDataSets.bbt_object_propertyRow rowLength = DBManager.Instance.projectDataset.bbt_object_property.Where(p => p.fk_object_type_id == deck.bbt_object_typeRow.pk_id && p.fk_properties_id == width.pk_id).First();
+            this.Size = new Size(Int32.Parse(rowWidth.value),Int32.Parse(rowLength.value));
+
         }
+
+
 
         public void addObject(BioBotDataSets.bbt_objectRow obj)
         {
-            Label Mod = new Label();
-            BioBotDataSets.bbt_object_propertyRow rowWidth = DBManager.Instance.projectDataset.bbt_object_property.Where(p => p.fk_object_type_id == _objectRow.bbt_object_typeRow.pk_id && p.fk_properties_id == 2).First();
-            BioBotDataSets.bbt_object_propertyRow rowLength = DBManager.Instance.projectDataset.bbt_object_property.Where(p => p.fk_object_type_id == _objectRow.bbt_object_typeRow.pk_id && p.fk_properties_id == 3).First();
-            Mod.Text = obj.pk_id.ToString();
-            Mod.Location = new Point(obj.deck_x, obj.deck_y);
-            Mod.Height = Int16.Parse(rowWidth.value);
-            Mod.Width = Int16.Parse(rowLength.value);
-            Mod.BorderStyle = BorderStyle.FixedSingle;
-            Mod.TextAlign = ContentAlignment.MiddleCenter;
-            
-            this.deck.Controls.Add(Mod);
+            if (obj.activated == "1")
+            {
+                Label Mod = new Label();
+                BioBotDataSets.bbt_object_propertyRow rowWidth = DBManager.Instance.projectDataset.bbt_object_property.Where(p => p.fk_object_type_id == obj.bbt_object_typeRow.pk_id && p.fk_properties_id == length.pk_id).First();
+                BioBotDataSets.bbt_object_propertyRow rowLength = DBManager.Instance.projectDataset.bbt_object_property.Where(p => p.fk_object_type_id == obj.bbt_object_typeRow.pk_id && p.fk_properties_id == width.pk_id).First();
+                Mod.Text = obj.pk_id.ToString();
+                Mod.Location = new Point(obj.deck_x, obj.deck_y);
+                Mod.Height = Int16.Parse(rowWidth.value);
+                Mod.Width = Int16.Parse(rowLength.value);
+                Mod.BorderStyle = BorderStyle.FixedSingle;
+                Mod.TextAlign = ContentAlignment.MiddleCenter;
+                this.Controls.Add(Mod);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (dataGridView1.SelectedRows.Count != 0)
-                _objectRow = getSelectedObjectRow();
-            addObject(_objectRow);
+
         }
 
         public Model.Data.BioBotDataSets.bbt_objectRow getSelectedObjectRow()
         {
             Model.Data.BioBotDataSets.bbt_objectRow objectRow = null;
 
-            if (bsBiobot.Current == null) return null;
-            if (!(bsBiobot.Current.GetType() == typeof(DataRowView))) return null;
-            DataRowView rowView = bsBiobot.Current as DataRowView;
+            if (bsObject.Current == null) return null;
+            if (!(bsObject.Current.GetType() == typeof(DataRowView))) return null;
+            DataRowView rowView = bsObject.Current as DataRowView;
             if (rowView.Row == null) return null;
             if (!(rowView.Row.GetType() == typeof(Model.Data.BioBotDataSets.bbt_objectRow))) return null;
             objectRow = rowView.Row as Model.Data.BioBotDataSets.bbt_objectRow;
             return objectRow;
+        }
+
+        private void DeckView_DragDrop(object sender, DragEventArgs e)
+        {
+            BioBotDataSets.bbt_objectRow row = e.Data.GetData(typeof(BioBotDataSets.bbt_objectRow)) as BioBotDataSets.bbt_objectRow;
+            addObject(row);
+        }
+
+        private void DeckView_DragEnter(object sender, DragEventArgs e)
+        {
+                e.Effect = DragDropEffects.Copy;
         }
     }
 }
