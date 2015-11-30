68fd4fa06c6f707e7b7b85f9a01279111e68b7e0
David Goulet <david.goulet4@usherbrooke.ca>
2015-11-30 17:45:40 -0500

    Added consumer pool funcitonality

---
+++ application/DLL/MultiChannel/MultiChannelPipette.cs
@@ -0,0 +1,30 @@
+﻿using BioBotCommunication.Serial.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioBotApp.DLL.MultiChannel
+{
+    public class MultiChannelPipette : Model.EventBus.Subscriber
+    {
+        private ConsumerPool consumerPool;
+
+        public MultiChannelPipette()
+        {
+
+        }
+
+
+        [Model.EventBus.Subscribe]
+        public void onExecuteEvent(Model.EventBus.Events.ExecutionService.ExecutionEvent e)
+        {
+            if (e.billboard == null) return;
+            consumerPool = new ConsumerPool(e.billboard);
+            consumerPool.newConsumer("OK4");
+            consumerPool.newConsumer("OK5");
+            consumerPool.startExecution();
+        }
+    }
+}
